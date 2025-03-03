25b7585a67db9e58ed5cb90b2035a8622f449dd8
Abhishek Singh <112750395+abhishek-singh09@users.noreply.github.com>
2025-03-03 21:39:19 +0530

    Time: 4 ms (80.60%) | Memory: 138.9 MB (21.38%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -2,23 +2,35 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         
-        vector<int>arr;
+        vector<int>less, equal, grater;
+        
         for(int i=0;i<nums.size();i++){
-            if(nums[i]<pivot){
-                arr.push_back(nums[i]);
-            }
+            if(nums[i]<pivot) less.push_back(nums[i]);
+            else if(nums[i]==pivot) equal.push_back(nums[i]);
+            else grater.push_back(nums[i]);
         }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==pivot){
-                arr.push_back(nums[i]);
-            }
-        }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>pivot){
-                arr.push_back(nums[i]);
-            }
-        }
-        return arr;
+        less.insert(less.end(),equal.begin(),equal.end());
+        less.insert(less.end(), grater.begin(), grater.end());    
+ 
+        return less;
+        
+        //vector<int>arr;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]<pivot){
+        //         arr.push_back(nums[i]);
+        //     }
+        // }
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==pivot){
+        //         arr.push_back(nums[i]);
+        //     }
+        // }
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]>pivot){
+        //         arr.push_back(nums[i]);
+        //     }
+        // }
+        // return arr;
         
     }
 };
