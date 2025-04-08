5c3488bd250df32a214a5cff399c9e3d8f7189fd
Abhishek Singh <112750395+abhishek-singh09@users.noreply.github.com>
2025-04-08 14:39:37 +0530

    Time: 2165 ms (5.01%) | Memory: 335.8 MB (31.69%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,80 +1,28 @@
-// class Solution {
-// public:
-//     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-
-//         vector<vector<int>> graph(n);
-//         for(auto& edge: edges){
-//             graph[edge[0]].push_back(edge[1]);
-//             graph[edge[1]].push_back(edge[0]);
-//         }
-
-//         vector<bool> visited(n, false);
-
-//         function<bool(int)> dfs = [&](int node){
-//             if(node==destination) return true;
-//             visited[node]=true;
-
-//             for(int neighbour: graph[node]){
-//                 if(!visited[neighbour]){
-//                     if(dfs(neighbour)) return true;
-//                 }
-//             }
-//             return false;
-//         };
-
-//         return dfs(source);     
-        
-//     }
-// };
-
-#include <vector>
-using namespace std;
-
-class Graph {
-private:
-    int nodes;
-    vector<vector<int>> adjList;
-    vector<bool> visited;
-
-public:
-    // Constructor to initialize the graph
-    Graph(int n, vector<vector<int>>& edges) {
-        nodes = n;
-        adjList.resize(n);
-        visited.resize(n, false);
-
-        // Build the adjacency list
-        for (auto& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            adjList[u].push_back(v);
-            adjList[v].push_back(u);
-        }
-    }
-
-    // DFS function
-    bool dfs(int current, int destination) {
-        if (current == destination) return true;
-        visited[current] = true;
-
-        for (int neighbor : adjList[current]) {
-            if (!visited[neighbor]) {
-                if (dfs(neighbor, destination)) return true;
-            }
-        }
-        return false;
-    }
-
-    // Public method to check path validity
-    bool hasValidPath(int source, int destination) {
-        return dfs(source, destination);
-    }
-};
-
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        Graph g(n, edges);  // create a graph object
-        return g.hasValidPath(source, destination);  // use its method
+
+        vector<vector<int>> graph(n);
+        for(auto& edge: edges){
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+
+        vector<bool> visited(n, false);
+
+        function<bool(int)> dfs = [&](int node){
+            if(node==destination) return true;
+            visited[node]=true;
+
+            for(int neighbour: graph[node]){
+                if(!visited[neighbour]){
+                    if(dfs(neighbour)) return true;
+                }
+            }
+            return false;
+        };
+
+        return dfs(source);     
+        
     }
 };
