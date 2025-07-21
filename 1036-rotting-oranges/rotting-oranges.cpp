e91d1a22c6318b80300a7df58863d8e596101e2a
Abhishek Singh <112750395+abhishek-singh09@users.noreply.github.com>
2025-07-21 10:42:02 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (97.42%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,64 +1,62 @@
 class Cell{
 public:
     int r, c, t;
-
-    Cell(int row, int col, int tim){
+    Cell(int row, int col, int tm){
         r=row;
         c=col;
-        t=tim;
+        t=tm;
     }
 };
 
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<Cell> q;
-        int rows=grid.size();
-        int cols=grid[0].size();
         int time=0;
-
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]==2) q.push(Cell(i,j,0));
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<Cell> que;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    que.push(Cell(i, j, 0));
+                }
             }
         }
 
-        while(!q.empty()){
+        while(!que.empty()){
+            Cell cell= que.front();
+            que.pop();
+            int rw=cell.r;
+            int co=cell.c;
+            int tim = cell.t;
+            time=max(time, tim);
             
-            Cell curr= q.front();
-            q.pop();
-            int r=curr.r;
-            int c=curr.c;
-            int t=curr.t;
-            
-            time=max(time,t);
+            if(rw-1>=0 && grid[rw-1][co]==1){
+                grid[rw-1][co]=2;
+                que.push(Cell(rw-1, co, tim+1));
+            }
+            if(co-1>=0 && grid[rw][co-1]==1){
+                grid[rw][co-1]=2;
+                que.push(Cell(rw, co-1, tim+1));
+            }
+            if(rw+1<n && grid[rw+1][co]==1){
+                grid[rw+1][co]=2;
+                que.push(Cell(rw+1, co, tim+1));
+            }
+            if(co+1<m && grid[rw][co+1]==1){
+                grid[rw][co+1]=2;
+                que.push(Cell(rw, co+1, tim+1));
+            }
 
-            if(r+1<rows && grid[r+1][c]==1){
-                q.push(Cell(r+1,c,t+1));
-                grid[r+1][c]=2;
-            }
-            if(r-1>=0 && grid[r-1][c]==1){
-                q.push(Cell(r-1,c,t+1));
-                grid[r-1][c]=2;
-            }
-            if(c+1<cols && grid[r][c+1]==1){
-                q.push(Cell(r, c+1,t+1));
-                grid[r][c+1]=2;
-            }
-            if(c-1>=0 && grid[r][c-1]==1){
-                q.push(Cell(r,c-1,t+1));
-                grid[r][c-1]=2;
-            }
         }
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]==1) return -1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
             }
         }
-
         return time;
-
-        
     }
 };
