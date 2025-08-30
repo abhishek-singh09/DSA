5b226908418da6aa9be461ff18da1420112152e6
Abhishek Singh <112750395+abhishek-singh09@users.noreply.github.com>
2025-08-30 22:25:36 +0530

    Time: 4 ms (52.67%) | Memory: 23.1 MB (62.58%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,42 +1,56 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        for(int i=0;i<9;i++){
-            vector<bool> seen(9,false);
-            for(int j=0;j<9;j++){
-                if(board[i][j]=='.') {
-                    continue;
-                }
-                int num=board[i][j]-'1';
-                if(seen[num]) return false;
-                seen[num]=true;
-            }
-        }        
-        for(int i=0;i<9;i++){
-            vector<bool> seen(9,false);
-            for(int j=0;j<9;j++){
-                if(board[j][i]=='.') continue;
-                int num=board[j][i]-'1';
-                if(seen[num]) return false;
-                seen[num]=true;
-            }
-        } 
+        // for(int i=0;i<9;i++){
+        //     vector<bool> seen(9,false);
+        //     for(int j=0;j<9;j++){
+        //         if(board[i][j]=='.') {
+        //             continue;
+        //         }
+        //         int num=board[i][j]-'1';
+        //         if(seen[num]) return false;
+        //         seen[num]=true;
+        //     }
+        // }        
+        // for(int i=0;i<9;i++){
+        //     vector<bool> seen(9,false);
+        //     for(int j=0;j<9;j++){
+        //         if(board[j][i]=='.') continue;
+        //         int num=board[j][i]-'1';
+        //         if(seen[num]) return false;
+        //         seen[num]=true;
+        //     }
+        // } 
 
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                vector<bool> seen(9,false);
-                for(int x=0;x<3;x++){
-                    for(int y=0;y<3;y++){
-                        int row=3*i+x;
-                        int col=3*j+y;
-                        if(board[row][col]=='.') continue;
-                        int num=board[row][col]-'1';
-                        if(seen[num]) return false;
-                        seen[num]=true;
-                    }
-                }
+        // for(int i=0;i<3;i++){
+        //     for(int j=0;j<3;j++){
+        //         vector<bool> seen(9,false);
+        //         for(int x=0;x<3;x++){
+        //             for(int y=0;y<3;y++){
+        //                 int row=3*i+x;
+        //                 int col=3*j+y;
+        //                 if(board[row][col]=='.') continue;
+        //                 int num=board[row][col]-'1';
+        //                 if(seen[num]) return false;
+        //                 seen[num]=true;
+        //             }
+        //         }
+        //     }
+        // }
+        // return true;       
+
+        vector<vector<bool>> row(9,vector<bool>(9,false));
+        vector<vector<bool>> col(9,vector<bool>(9,false));
+        vector<vector<bool>> boxes(9,vector<bool>(9,false));
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
+                int num=board[i][j]-'1';
+                int r=(i/3)*3+(j/3);
+                if(row[i][num] || col[j][num] || boxes[r][num]) return false;
+                row[i][num] = col[j][num] = boxes[r][num]=true;
             }
         }
-        return true;       
+        return true;
     }
 };
