effc9bfde5d82b12142681a197a9bb9834258796
Abhishek Singh <112750395+abhishek-singh09@users.noreply.github.com>
2025-09-04 15:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (55.11%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int a=0;
+        int b=0;
+        if(x>z){
+            while(x>=z){
+                a++;
+                x--;
+            }
+        }else if(x<z){
+            while(x<=z){
+                a++;
+                x++;
+            }
+        }
+
+        if(y>z){
+            while(y>=z){
+                b++;
+                y--;
+            }
+        }else if(y<z){
+            while(y<=z){
+                b++;
+                y++;
+            }
+        }
+        
+        if(a==b) return 0;
+        if(a<b) return 1;
+
+        return 2;
+
+        
+        
+    }
+};
